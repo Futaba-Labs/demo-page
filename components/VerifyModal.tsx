d3f47ad12e0ba8f4ea3613311d68123d9164b5a1
adachi_440 <tomoki1999314@gmail.com>
2023-10-03 03:04:59 +0800

    fix verify modal

--- components/VerifyModal.tsx
+++ components/VerifyModal.tsx
@@ -1,7 +1,6 @@
-import { Modal, ModalHeader, ModalBody, ModalContent, Button } from '@nextui-org/react'
+import { Modal, ModalHeader, ModalContent } from '@nextui-org/react'
 import { NextPage } from 'next'
 import { Image } from '@nextui-org/react'
-import { useChainModal, useConnectModal } from '@rainbow-me/rainbowkit'
 import { useAccount, useContractRead, useNetwork } from 'wagmi'
 import { useEffect, useMemo } from 'react'
 import { LIGHT_CLIENT_ABI } from 'utils'
@@ -13,9 +12,6 @@ interface Props {
   onClose: () => void
 }
 const VerifyModal: NextPage<Props> = ({ isOpen, onOpenChange, onClose }) => {
-  const { openConnectModal } = useConnectModal()
-  const { openChainModal } = useChainModal()
-
   const { address } = useAccount()
   const { chain } = useNetwork()
 
@@ -62,37 +58,8 @@ const VerifyModal: NextPage<Props> = ({ isOpen, onOpenChange, onClose }) => {
           <>
             <ModalHeader className='flex flex-col place-items-center'>
               <Image height={100} width={150} src={'/images/futaba_512.png'} alt='Default Image' />
-              <h2 className='text-2xl font-semibold mb-4'>Welcome to Futaba 🌱</h2>
+              <h2 className='text-2xl font-semibold mb-4'>Comming soon 🌱</h2>
             </ModalHeader>
-            <ModalBody>
-              <>
-                <p className='text-md font-normal text-center'>{'Futaba is currently in private beta.'}</p>
-                <p className='text-md font-normal text-center'>
-                  {'If you would like to try the demo, please apply for private beta access.'}
-                </p>
-                {!allowed && chain?.id == 80001 ? (
-                  <p className='text-md font-normal mb-3 text-center'>
-                    Access is not allowed. Please click the button below to apply for Private beta access.
-                  </p>
-                ) : (
-                  <div className='mb-1'>
-                    {openConnectModal && (
-                      <Button onPress={openConnectModal} color='success' fullWidth={true}>
-                        Wallet connect
-                      </Button>
-                    )}
-                    {openChainModal && (
-                      <Button onPress={openChainModal} color='success' fullWidth={true}>
-                        Change chain
-                      </Button>
-                    )}
-                  </div>
-                )}
-                <Button className='mb-3' color='success'>
-                  Apply to private beta
-                </Button>
-              </>
-            </ModalBody>
           </>
         )}
       </ModalContent>
