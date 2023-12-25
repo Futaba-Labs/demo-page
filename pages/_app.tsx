64b26aabcb812386c34ad6431fa65ad6db0ad4b2
adachi_440 <tomoki1999314@gmail.com>
2023-12-25 17:21:15 +0900

    add coin98 wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,14 @@ import '../styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react'
 import '@rainbow-me/rainbowkit/styles.css'
 
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { connectorsForWallets, getDefaultWallets } from '@rainbow-me/rainbowkit'
+import {
+  coin98Wallet,
+  injectedWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi'
 import { polygonMumbai } from 'wagmi/chains'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
@@ -21,11 +28,19 @@ const { chains, publicClient } = configureChains(
   ],
 )
 
-const { connectors } = getDefaultWallets({
-  appName: 'Futaba demo app',
-  projectId: '5573a7c23be46c0343267fb1dca563af',
-  chains,
-})
+const projectId = '5573a7c23be46c0343267fb1dca563af'
+
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [
+      metaMaskWallet({ projectId, chains }),
+      coin98Wallet({ projectId, chains }),
+      rainbowWallet({ projectId, chains }),
+      walletConnectWallet({ projectId, chains }),
+    ],
+  },
+])
 
 const config = createConfig({
   autoConnect: true,
